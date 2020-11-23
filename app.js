185bc4d73cabe9c28c9818317ef7ad158687b59a
Q <coderinch@gmail.com>
2020-11-23 12:41:22 +0900

    randomize fact and get human data from form

--- app.js
+++ app.js
@@ -5,21 +5,28 @@ function getJson() {
       return res.json();
     })
     .then((data) => {
-      generateTile(data);
+      const dino_arr = data.Dinos.map(
+        (el) => new Dino(el.species, el.height, el.weight, el.diet, el.fact)
+      );
+      generateTile(dino_arr);
     });
 }
 
 // Create Dino Constructor
-function Dino(species, height, weight, diet) {
-  this.species = species;
-  this.height = height;
-  this.weight = weight;
-  this.diet = diet;
+class Dino {
+  constructor(species, height, weight, diet, fact) {
+    this.species = species;
+    this.height = height;
+    this.weight = weight;
+    this.diet = diet;
+    this.fact = fact;
+  }
 }
 // Create Dino Objects
+let dino = new Dino();
 
 // Create Human Object
-const human = new Object();
+let human = new Object();
 
 //input DOM
 const name = document.getElementById("name");
@@ -29,20 +36,62 @@ const weight = document.getElementById("weight");
 const diet = document.getElementById("diet");
 
 // Use IIFE to get human data from form
+const getHumanData = function () {
+  (function () {
+    human.species = name.value;
+    human.height = parseInt(feet.value) * 12 + parseInt(inches.value);
+    human.weight = weight.value;
+    human.diet = diet.value;
+  })(name, feet, inches, weight, diet);
+};
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
-
+Dino.prototype.compareHeight = function () {
+  console.log("this is height comparison");
+};
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
-
+Dino.prototype.compareWeight = function () {
+  console.log("this is weight comparison");
+};
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
 
+//randomize fact
+let randomized_fact = [];
+
+function randomFact(data) {
+  //extracting fact element from Dino array
+  data.forEach((el) => {
+    randomized_fact.push(el.fact);
+  });
+  //randomize by sort
+  randomized_fact.sort(() => Math.random() - 0.5);
+  //remove pigeon's fact from randomized fact array
+  const index = randomized_fact.indexOf("All birds are living dinosaurs.");
+  randomized_fact.splice(index, 1);
+}
 // Generate Tiles for each Dino in Array
 function generateTile(data) {
-  const array_dino = data.Dinos;
-  console.log(human);
+  randomFact(data);
+  //insert randomized fact into new dino data
+  let updatedDino = [];
+  let count = 0;
+  data.forEach((el) => {
+    dino.species = el.species;
+    dino.height = el.height;
+    dino.weight = el.weight;
+    dino.diet = el.diet;
+    if (el.species === "Pigeon") {
+      dino.fact = "All birds are living dinosaurs.";
+    } else {
+      dino.fact = randomized_fact[count];
+      count++;
+    }
+    updatedDino.push(JSON.parse(JSON.stringify(dino)));
+  });
+  let array_dino = updatedDino;
   array_dino.splice(4, 0, human);
   array_dino.forEach((el) => {
     addToDom(el);
@@ -87,6 +136,7 @@ function resetValue() {
 //compare Me eventlistner
 function compareWithDino() {
   getJson();
+  getHumanData(name, feet, inches, weight, diet);
   removeForm();
   resetValue();
 }
